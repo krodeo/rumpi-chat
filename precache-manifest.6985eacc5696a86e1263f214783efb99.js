77d6eb8ba9f169b5e2f7b215e757f88e62a2e744
Ainul Yaqin <itsyaqin@gmail.com>
2020-09-29 20:24:09 +0700

    Updates

---
+++ precache-manifest.6985eacc5696a86e1263f214783efb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c50047eddfee2563104bae2c9402707",
+    "url": "/rumpi-chat/index.html"
+  },
+  {
+    "revision": "f75b960631f88e932ed3",
+    "url": "/rumpi-chat/static/css/main.636b71d5.chunk.css"
+  },
+  {
+    "revision": "4265ae73914a8ce09a1f",
+    "url": "/rumpi-chat/static/js/2.38ed8b45.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/rumpi-chat/static/js/2.38ed8b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75b960631f88e932ed3",
+    "url": "/rumpi-chat/static/js/main.196aac1b.chunk.js"
+  },
+  {
+    "revision": "2b6eeda2eca99f752678",
+    "url": "/rumpi-chat/static/js/runtime-main.f797f809.js"
+  }
+]);
